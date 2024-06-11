bbd255d29e467460ed3f7077a67ed571ae56cbce
gang.qiang <gang.qiang@intasect.co.jp>
2024-06-11 18:58:09 +0900

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,19 @@
-import { CatsModule } from './cats/cats.module';
+// import { CatsModule } from './cats/cats.module';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://root:1@192.168.99.41:27017/test?authSource=admin',
-      {
-        connectionFactory: (connection) => {
-          console.log('MongoDB connected successfully!');
-          return connection;
-        },
-      },
-    ),
-    CatsModule,
+    // MongooseModule.forRoot(
+    //   'mongodb://root:1@192.168.99.41:27017/test?authSource=admin',
+    //   {
+    //     connectionFactory: (connection) => {
+    //       console.log('MongoDB connected successfully!');
+    //       return connection;
+    //     },
+    //   },
+    // ),
+    // CatsModule,
   ],
 })
 export class AppModule {}
