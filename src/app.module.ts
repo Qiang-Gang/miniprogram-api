6a421b2848e8e0c186b8f306e3dc42f9685fe05e
gang.qiang <gang.qiang@intasect.co.jp>
2024-06-11 10:16:19 +0900

    Initial commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://root:1@localhost:27017/test?authSource=admin',
+      'mongodb://root:1@192.168.99.41:27017/test?authSource=admin',
       {
         connectionFactory: (connection) => {
           console.log('MongoDB connected successfully!');
